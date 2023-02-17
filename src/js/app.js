9507e1accdb328c1253709d42d1908d262274d69
Sallai Andras <andrewteki@gmail.com>
2023-02-17 11:11:53 +0100

    Kész

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,14 @@
-const etulaj = document.querySelector("#etulaj");
 
-etulaj.addEventListener('click', () => {
-    console.log(etulaj.checked)
+const ownerRadio = document.querySelector('#owner');
+const renterRadio = document.querySelector('#renter');
+const cusRenterRadio = document.querySelector('#cusRenter');
+const subCusRenterRadio = document.querySelector('#subCusRenter');
+
+ownerRadio.addEventListener('click', () => {
+    console.log('Az eladó egy tulajdonos');
+    cusRenterRadio.checked = true;
+});
+renterRadio.addEventListener('click', () => {
+    console.log('Az eladó eleve bérlő');
+    subCusRenterRadio.checked = true;
 });
